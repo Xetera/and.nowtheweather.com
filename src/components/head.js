511d020f0c2cc232ef539fa9d982f057cc829e58
Xetera <mythologyali@gmail.com>
2019-02-13 14:51:06 -0800

    theme color added

--- src/components/head.js
+++ src/components/head.js
@@ -8,6 +8,7 @@ export const SiteHead = () =>
 		<title>And now, the weather</title>
 		<link rel="icon" href={favicon}/>
 		<meta name="description" content="A collection of all songs in Welcome to Nightvale"/>
+		<meta name="theme-color" content="#9a3b8f"/>
 		<meta property="og:type" content="website"/>
 		<meta property="og:title" content="And now, the weather!"/>
 		<meta property="og:description" content="A collection of all songs in Welcome to Nightvale"/>
