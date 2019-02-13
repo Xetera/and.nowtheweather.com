37aaaff72f954ca1cf17d117116fcb84feea4c85
Xetera <mythologyali@gmail.com>
2019-02-13 13:55:29 -0800

    production

--- static.config.js
+++ static.config.js
@@ -2,7 +2,7 @@ import { fetchWeather } from "./src/scrape";
 
 
 export default {
-  siteRoot: "http://localhost:3000",
+  siteRoot: process.env.NODE_ENV === "production" ? "http://localhost:3000" : "https://nowtheweather.com",
   getSiteData: async () => {
     return ({
       weather: await fetchWeather(),
